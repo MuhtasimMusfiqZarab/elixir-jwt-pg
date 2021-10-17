c279cedfdbefb815e19fdffec6eba388a7d70945
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-10-18 01:31:46 +0600

    add migration script

--- priv/repo/migrations/20211017192048_create_user.exs
+++ priv/repo/migrations/20211017192048_create_user.exs
@@ -2,6 +2,10 @@ defmodule Users.Repo.Migrations.CreateUser do
   use Ecto.Migration
 
   def change do
-
+    create table(:users) do
+      add(:name, :string)
+      add(:password, :string)
+      add(:token, :string)
+    end
   end
 end
